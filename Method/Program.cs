387316903e976003b134268dd7069b89f472db26
Ferdi <Ferdi@DESKTOP-53SEIK5>
2022-06-15 13:37:25 +0300

    Add project files.

---
+++ Method/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun urun1 = new Urun();
+            urun1.Id = 850800;
+            urun1.Adi = "elma";
+            urun1.Fiyat = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Id = 880000;
+            urun2.Adi = "karpuz";
+            urun2.Fiyat = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            foreach (Urun x in urunler)
+            {
+                Console.WriteLine(x.Adi);
+                Console.WriteLine(x.Fiyat);
+                Console.WriteLine(x.Aciklama);
+                Console.WriteLine("***********");
+
+
+            }
+
+            SepetManager sepeteEkle = new SepetManager();
+            sepeteEkle.Ekle(urun1);
+            sepeteEkle.Ekle(urun2);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Method/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+     class SepetManager
+    {
+        public void Ekle(Urun y)
+        {
+            Console.WriteLine(y.Adi+" ürünü sepete eklendi");
+          
+
+
+
+        }
+
+    }
+}
